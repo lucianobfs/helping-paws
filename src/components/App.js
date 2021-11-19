d22bf7eb1d234f97b65b0ced97f550d2d510f018
Luciano <lucianobfsantana@hotmail.com>
2021-11-19 15:34:38 -0300

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link, Routes, Route, Navigate } from "react-router-dom";
+import axios from "axios";
+
+
+function App() {
+  return (
+    <div >
+      oi teste
+    </div>
+  );
+}
+
+export default App;
