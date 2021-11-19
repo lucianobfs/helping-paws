25b8bfa5de1e7fa1511c07794da2b85bcd6027e6
Luciano <lucianobfsantana@hotmail.com>
2021-11-19 16:31:54 -0300

    routes small change in app component

---
+++ src/components/AddPet.js
@@ -0,0 +1,17 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link, Routes, Route, Navigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+import Home from './Home';
+import NavBar from './NavBar';
+import AllPets from './AllPets';
+import EditPet from './EditPet';
+
+function AddPet() {
+
+}
+
+
+
+export default AddPet
